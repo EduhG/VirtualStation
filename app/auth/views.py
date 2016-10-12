a35578e72f1a3ebc7920f6f6e914407f70a4d751
eduhg <edwinnyangena@gmail.com>
2016-10-12 12:27:12 +0300

    updated auth/views with signin route

---
+++ app/auth/views.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, session, url_for, redirect
+from forms import SigninForm
+from . import auth
+
+
+@auth.route('/signin', methods=['GET', 'POST'])
+def signin():
+    form = SigninForm()
+
+    if 'loginid' in session:
+        print session['loginid']
+        return redirect(url_for('home.index'))
+
+    if request.method == 'POST':
+        if form.validate() is False:
+            return render_template('auth/signin.html', form=form)
+        else:
+            session['loginid'] = form.email.data
+            return redirect(url_for('home.index'))
+
+    elif request.method == 'GET':
+        return render_template('auth/signin.html', form=form)
