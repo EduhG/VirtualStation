f2da2957bd82c679b3fd88e3932e55efa8e0a4b1
eduhg <edwinnyangena@gmail.com>
2016-10-17 16:12:12 +0300

    updated reportedcases model

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -3,8 +3,28 @@ from .. import db
 
 class ReportedCase(db.Model):
     __tablename__ = 'reported_cases'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
+    id = db.Column(db.Integer, primary_key=True)
+    id_method = db.Column(db.String(100))
+    id_number = db.Column(db.String(64), unique=True)
+    first_name = db.Column(db.String(100))
+    other_names = db.Column(db.String(100))
+    gender = db.Column(db.String(20))
+    phone_number = db.Column(db.String(20))
+    email = db.Column(db.String(64), unique=True)
+    reg_date = db.Column(db.String(20))
+    complaint_type = db.Column(db.String(100))
+    description = db.Column(db.String(1000))
+
+    def __init__(self, id_method, id_number, first_name, other_names, gender,
+                 phone_number, email, reg_date, complaint_type, description):
+        self.id_method = id_method
+        self.id_number = id_number
+        self.first_name = first_name
+        self.other_names = other_names
+        self.gender = gender
+        self.phone_number = phone_number
+        self.email = email
+        self.reg_date = reg_date
+        self.complaint_type = complaint_type
+        self.description = description
