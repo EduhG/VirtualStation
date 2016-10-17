f19dcb7eed22caba887a4f19adbf59348ad2276b
eduhg <edwinnyangena@gmail.com>
2016-10-17 08:11:04 +0300

    updated charts

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -17,7 +17,7 @@ var myChart = new Chart(ctx, {
             {
                 label: "",
                 fill: false,
-                lineTension: 0.1,
+                lineTension: 0.5,
                 backgroundColor: "rgba(75,192,192,0.4)",
                 borderColor: "rgba(75,192,192,1)",
                 borderCapStyle: 'butt',
@@ -31,7 +31,7 @@ var myChart = new Chart(ctx, {
                 pointHoverBackgroundColor: "rgba(75,192,192,1)",
                 pointHoverBorderColor: "rgba(220,220,220,1)",
                 pointHoverBorderWidth: 2,
-                pointRadius: 1,
+                pointRadius: 5,
                 pointHitRadius: 10,
                 data: [65, 59, 80, 81, 56, 55, 40],
                 spanGaps: false,
