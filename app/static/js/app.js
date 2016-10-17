9c0c13465dc745f47646c0a243dd9a32df5b9595
eduhg <edwinnyangena@gmail.com>
2016-10-17 10:30:10 +0300

    added new files

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -4,7 +4,7 @@
 
 $('.button-collapse').sideNav({
     menuWidth: 300, // Default is 240
-    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
 });
 $('.collapsible').collapsible();
 
