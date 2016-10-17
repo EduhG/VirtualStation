e473efbb32c876a18e6977f929a335a71e0a444c
eduhg <edwinnyangena@gmail.com>
2016-10-17 16:52:19 +0300

    updated new cases view on post method

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, url_for, redirect, flash
 from flask_login import login_required, login_user
 from forms import NewCaseForm
+from .. import db
 from .models import ReportedCase
 from . import dashboard
 
@@ -17,11 +18,24 @@ def newcase():
     form = NewCaseForm()
 
     if form.validate_on_submit():
-        """user = ReportedCase.query.filter_by(email=form.email.data).first()
-        if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remember_me.data)
+        id_method = request.form['id_method']
+        id_number = request.form['id_number']
+        first_name = request.form['first_name']
+        other_names = request.form['other_names']
+        gender = request.form['gender']
+        phone_number = request.form['phone_number']
+        email = request.form['email']
+        reg_date = request.form['reg_date']
+        complaint_type = request.form['complaint_type']
+        description = request.form['description']
+
+        newstudent = ReportedCase(id_method, id_number, first_name, other_names, gender,
+                                  phone_number, email, reg_date, complaint_type, description)
+        db.session.add(newstudent)
+        db.session.commit()
+
+        flash('Invalid username or password.')
         return redirect(request.args.get('next') or url_for('dashboard.newcase'))
-        flash('Invalid username or password.')"""
-        pass
+
     return render_template('dashboard/new-case.html', form=form)
 
