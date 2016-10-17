0a8d42b3d7880c67001133e24386d085a5c8117e
eduhg <edwinnyangena@gmail.com>
2016-10-17 12:56:17 +0300

    updated new case.html

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -2,6 +2,22 @@
  * Created by eduhg on 10/13/16.
  */
 
+$(document).ready(function () {
+    $('select').material_select();
+
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+});
+
+$('select').material_select();
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+});
+
 $('.button-collapse').sideNav({
     menuWidth: 300, // Default is 240
     closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
