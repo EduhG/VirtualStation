4f3defcc162d74d1db03992b086faa53adc84e37
eduhg <edwinnyangena@gmail.com>
2016-10-18 18:18:50 +0300

    added first and last days of each month to an array

--- app/utils/custom_calendar.py
+++ app/utils/custom_calendar.py
@@ -2,12 +2,23 @@ from datetime import date, datetime, timedelta
 import calendar
 
 
-def months_days():
+def months_days(current_year):
     month_days = []
 
-    pass
+    first_dates = [first_day(str(current_year) + "-" + str(i + 1) + "-" + str(1)) for i in range(12)]
+    last_dates = [datetime(first_date.year, first_date.month, calendar.mdays[first_date.month])
+                  for first_date in first_dates]
+    months = ['January', 'February', 'March', 'April', 'May', 'June',
+              'July', 'August', 'September', 'October', 'November', 'December']
+
+    for date1 in last_dates:
+        print date1
 
     return month_days
+
+
+def first_day(str_date):
+    return datetime.strptime(str_date, '%Y-%m-%d').date()
 
 
 def last_day_of_previous_month(dt):
@@ -23,3 +34,7 @@ def last_day_of_current_month():
 
 def first_day_of_current_month(dt):
     return date(dt.year, dt.month, 1)
+
+
+if __name__ == "__main__":
+    print months_days(2016)
