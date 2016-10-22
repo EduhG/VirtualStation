4006ba4aae926991ffda822e19b974aed2b00e2f
eduhg <edwinnyangena@gmail.com>
2016-10-22 10:14:06 +0300

    created user signup form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,3 +8,15 @@ class SigninForm(Form):
     remember_me = BooleanField('remember_me', default=False)
 
     submit = SubmitField("Sign In")
+
+
+class SignupForm(Form):
+    first_name = StringField('First Name', [validators.DataRequired("Please enter your user name.")])
+    other_names = StringField('Other Names', [validators.DataRequired("Please enter your user name.")])
+    email = StringField("Email", [validators.DataRequired("Please enter your email address."),
+                        validators.Email("Please enter your email address.")])
+    username = StringField('User Name', [validators.DataRequired("Please enter your user name.")])
+    password = PasswordField('Password', [validators.DataRequired("Please enter a password.")])
+    password_again = PasswordField('Confirm Password', [validators.DataRequired("Please enter a password.")])
+
+    submit = SubmitField("Create Account")
